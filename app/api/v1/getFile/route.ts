a7acfaa7fcbdc59c6eec08a59da6417f5150bfa6
Danng <futuretech.coder4@gmail.com>
2026-09-15 07:26:27 +0100

    Update filename to make replacement more obvious

--- app/api/v1/getFile/route.ts
+++ app/api/v1/getFile/route.ts
@@ -14,8 +14,8 @@ interface payload {
 
 const versions:version[] = [
     {
-        fileName: "splink-1.0.0.jar",
-        version: "1.0.1",
+        fileName: "splink-1.1.0.jar",
+        version: "1.1.0",
         sha256: "5c4d4e996c8830cd4f85a9748fd1e741aaeaf41ab51aed3e5ef5290a0dfb2cce"
     }
 ]
